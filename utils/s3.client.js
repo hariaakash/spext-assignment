fa0ca3f7598ff9e2891be71942840a1d8fed7d72
Hari <smgdark@gmail.com>
2022-06-13 21:19:54 +0530

    added s3 client

---
+++ utils/s3.client.js
@@ -0,0 +1,20 @@
+const Minio = require('minio');
+
+const endPoint = process.env.S3_URI || 'storage.googleapis.com';
+const accessKey = process.env.S3_ACCESS_KEY;
+const secretKey = process.env.S3_SECRET_KEY;
+const region = process.env.S3_REGION;
+const bucket = process.env.S3_BUCKET;
+
+const minioClient = new Minio.Client({
+  endPoint,
+  useSSL: true,
+  accessKey,
+  secretKey,
+  region,
+});
+
+module.exports = {
+  bucket,
+  client: minioClient,
+};
